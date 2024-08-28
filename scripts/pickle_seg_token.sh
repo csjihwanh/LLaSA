14b3b3be55a0ca41fd41b68aac8be3dfb2c73990
csjihwanh <csjihwanh@gmail.com>
2024-08-28 04:50:09 +0000

    segmentation configuration

--- scripts/pickle_seg_token.sh
+++ scripts/pickle_seg_token.sh
@@ -1,8 +1,10 @@
 echo "PYTHONPATH: ${PYTHONPATH}"
+echo "PYTHONPATH: /workspace/LLaSA/model/segmentation"
 which_python=$(which python)
 echo "which python: ${which_python}"
 export PYTHONPATH=${PYTHONPATH}:${which_python}
 export PYTHONPATH=${PYTHONPATH}:.
+export PYTHONPATH=$PYTHONPATH:/workspace/LLaSA/
 export CUDA_VISIBLE_DEVICES=0
 
 echo "PYTHONPATH: ${PYTHONPATH}"
@@ -11,8 +13,10 @@ NNODE=1
 NUM_GPUS=1
 NUM_CPU=128
 
+:'
 torchrun \
     --nnodes=${NNODE} \
     --nproc_per_node=${NUM_GPUS} \
-    --rdzv_backend=c10d \
-    model/segmentation/token_pickler.py
+    --rdzv_backend=c10d \'
+
+python model/segmentation/token_pickler.py > log.txt 2>&1 &
